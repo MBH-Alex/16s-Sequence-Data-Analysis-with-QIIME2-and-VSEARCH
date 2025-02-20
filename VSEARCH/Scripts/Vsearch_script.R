a70d94acaf0933b3f794df3f7b68abd1d2e42783
Alex Kidangathazhe <100247591+MBH-Alex@users.noreply.github.com>
2025-02-20 17:03:10 -0600

    Update Vsearch_script.R

--- VSEARCH/Scripts/Vsearch_script.R
+++ VSEARCH/Scripts/Vsearch_script.R
@@ -3,14 +3,14 @@ library(tidyverse)
 library(dplyr)
 library(stringr)
 
-Table <- fread("/Users/alexkid/Desktop/vsearch/all.otutab.txt")
-Taxonomy <- read.delim("/Users/alexkid/Desktop/vsearch/ASV_tax_raw.txt", sep="\t", header=F)
+Table <- fread("YOUR PATH HERE/all.otutab.txt")
+Taxonomy <- read.delim("YOUR PATH HERE/tax_raw.txt", sep="\t", header=F)
 Taxonomy$V1 = gsub(";.*","",Taxonomy$V1)
 Taxonomy$V6 = lapply(Taxonomy$V1, gsub, pattern = "OTU_", replacement = "")
 Taxonomy <- as.data.frame(Taxonomy)
 Taxonomy$V6 <- unlist(Taxonomy$V6)
-write.csv(Taxonomy, "/Users/alexkid/Desktop/vsearch/Taxonomy.csv", row.names = FALSE)
-Taxonomy <- read.csv("/Users/alexkid/Desktop/vsearch/Taxonomy.csv")
+write.csv(Taxonomy, "YOUR PATH HERE/axonomy.csv", row.names = FALSE)
+Taxonomy <- read.csv("YOUR PATH HERE/Taxonomy.csv")
 names(Taxonomy)[1] <- 'OTU'
 names(Taxonomy)[2] <- 'Taxonomy'
 names(Taxonomy)[3] <- 'Useless'
@@ -46,8 +46,8 @@ names(Table)[1] <- 'OTU'
 Table$Sort = lapply(Table$OTU, gsub, pattern = "OTU_", replacement = "")
 Table <- as.data.frame(Table)
 Table$Sort <- unlist(Table$Sort)
-write.csv(Table, "/Users/alexkid/Desktop/vsearch/OTU_Table.csv", row.names = FALSE)
-Table <- read.csv("/Users/alexkid/Desktop/vsearch/OTU_Table.csv")
+write.csv(Table, "YOUR PATH HERE/OTU_Table.csv", row.names = FALSE)
+Table <- read.csv("YOUR PATH HERE/OTU_Table.csv")
 Table <- Table[order(Table$Sort) , ]
 
 OTU_Table = Table
