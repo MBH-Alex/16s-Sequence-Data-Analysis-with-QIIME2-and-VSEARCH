ca32433308f6c3950c2b55cfa6b6ccdb8869aec7
Alex Kidangathazhe <100247591+MBH-Alex@users.noreply.github.com>
2025-02-20 17:01:39 -0600

    Update and rename Vsearch_script.Rmd to Vsearch_script.R

--- VSEARCH/Scripts/Vsearch_script.R
+++ VSEARCH/Scripts/Vsearch_script.R
@@ -1,19 +1,3 @@
----
-title: "Vsearch_Table"
-output: html_document
----
-
-```{r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE)
-```
-
-## R Markdown
-
-This is an R Markdown document. Markdown is a simple formatting syntax for authoring HTML, PDF, and MS Word documents. For more details on using R Markdown see <http://rmarkdown.rstudio.com>.
-
-When you click the **Knit** button a document will be generated that includes both content as well as the output of any embedded R code chunks within the document. You can embed an R code chunk like this:
-
-```{r cars}
 library(data.table)
 library(tidyverse)
 library(dplyr)
@@ -71,14 +55,3 @@ OTU_Table$OTU = Taxonomy3$Species
 names(OTU_Table)[1] <- 'Species'
 
 OTU_Table2 <- OTU_Table %>% group_by(Species) %>% summarise_each(funs(max)) 
-```
-
-## Including Plots
-
-You can also embed plots, for example:
-
-```{r pressure, echo=FALSE}
-plot(pressure)
-```
-
-Note that the `echo = FALSE` parameter was added to the code chunk to prevent printing of the R code that generated the plot.
